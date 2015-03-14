9d04034fa7442c0391e631aaa0fa6152b918fa53
VitalyP <ariekdev@gmail.com>
2015-03-13 21:10:47 -0400

    codeanywhereIdecommit

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ ParallaxRails::Application.routes.draw do
     end
   end
 
-  root 'demos#isite'
+  root 'demos#joy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
