21c3c5bcffcc53e120ef320d176fb645c5988dc4
vitalyp <ariekdev@gmail.com>
2015-06-18 18:41:22 +0300

    compile assets if missed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ ParallaxRails::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  ##config.assets.debug = true
-  config.assets.compile = false
+  config.assets.debug = true
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
